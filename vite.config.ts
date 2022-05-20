9b74a721760baf62474ab11a077b1a276fb6b9df
DiegoSilvadv <60052506+DiegoSilvadv@users.noreply.github.com>
2022-05-19 22:26:34 -0300

    service worker

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    VitePWA({})],
+    VitePWA({
+      strategies: 'injectManifest',
+    })],
   
 })
